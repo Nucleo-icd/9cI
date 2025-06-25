ddb92e97b30af003f3586bcb8fb2bde480aa556f
Nucleo-icd <nucleodeinformacaohspe@iamspe.sp.gov.br>
2025-06-25 09:10:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-
-document.getElementById('login-form').addEventListener('submit', async function (e) {
-  e.preventDefault();
-
+document.getElementById('login-button').addEventListener('click', async function () {
   const username = document.getElementById('username').value.trim().toLowerCase();
   const password = document.getElementById('password').value.trim().toLowerCase();
 
   if (username === 'ambulatorio' && password === 'ambulatorio') {
     try {
-      const response = await fetch("https://script.google.com/macros/s/AKfycbwWU8J-He1l9V-6STrNYc22Io3PJquOLXkge-PBrMsIqd1yWTzJh1a02aWZqOpZSsclxQ/exec", {
+      const response = await fetch("https://script.google.com/macros/s/AKfycbwVOSZnHUeKfRUyxy66QGegekodo8H4Hsf1efk2awz2w7ZCCjTmT8hSww2HEJ4cJOMZ6Q/exec", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded"
         },
         body: new URLSearchParams({
-          setor: "Anatomia Patológica"
+          setor: "Centro de Infusão"
         })
       });
 
@@ -26,7 +23,7 @@ document.getElementById('login-form').addEventListener('submit', async function 
       console.error("Erro na comunicação com a API:", error);
     }
 
-    window.location.href = "https://app.powerbi.com/view?r=eyJrIjoiMWNjY2FhOTMtNGQ4OS00YzRkLThlNjItZjQ5Yjc5MmU1YzU0IiwidCI6IjNhNzhiMGNkLTdjOGUtNDkyOS04M2Q1LTE5MGE2Y2MwMTM2NSJ9";
+    window.location.href = "https://app.powerbi.com/view?r=eyJrIjoiMmE4OTVhOWEtNjQ5Zi00NWE5LWIxZWItMTY1YzU0NzNlOTVjIiwidCI6IjNhNzhiMGNkLTdjOGUtNDkyOS04M2Q1LTE5MGE2Y2MwMTM2NSJ9";
   } else {
     document.getElementById('error-message').style.display = "block";
   }
